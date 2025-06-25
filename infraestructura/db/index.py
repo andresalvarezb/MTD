37c28effb7d5f84d7a5bd260231118d886a44a68
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2025-06-25 08:51:55 -0500

    docs: :rocket: se agregan variables de la ruta a la base de datos

--- infraestructura/db/index.py
+++ infraestructura/db/index.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -5,8 +6,12 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 load_dotenv()
-
-DATABASE_URL = "mysql+pymysql://root:Abecerra321$@localhost:3306/cuentasmedicas"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}$@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 print("🔗 DATABASE_URL:", DATABASE_URL)
