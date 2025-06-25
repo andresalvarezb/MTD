6a8f1e028e54effe480420ccdc8965ca4ef3a110
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2025-06-25 09:14:51 -0500

    fix: :bug: correcion en el llamado a las variables de entorno

--- infraestructura/db/index.py
+++ infraestructura/db/index.py
@@ -11,7 +11,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}$@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}$@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 print("🔗 DATABASE_URL:", DATABASE_URL)
