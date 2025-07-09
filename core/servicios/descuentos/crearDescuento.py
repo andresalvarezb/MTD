1de1297bb84eaad1e0085b574c24f3c2d0e32732
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2025-07-09 15:37:03 -0500

    feat: :sparkles: Separacion de respondabilidades en CrearDescuento
    Se divide la creacion del descuento y la actualizacion de la cuenta por pagar asociada al descuento

--- core/servicios/descuentos/crearDescuento.py
+++ core/servicios/descuentos/crearDescuento.py
@@ -17,12 +17,20 @@ class CrearDescuento:
         self.repo_actualizar_cuenta: ActualizarCuentaPorPagarProtocol = repo_cuenta
 
     def ejecutar(self, datos: CrearDescuentoDTO) -> Descuento:
+        descuento = self._crear_descuento(datos)
+
+        # * Actualización cuenta por pagar
+        self._actualizar_cuenta_por_pagar(descuento)
+
+        return descuento
+
+    def _crear_descuento(self, datos: CrearDescuentoDTO) -> Descuento:
         descuento = Descuento(
             id_usuario=datos.id_usuario,
             id_cuenta_por_pagar=datos.id_cuenta_por_pagar,
             id_deuda=datos.id_deuda,
             valor=datos.valor,
-            fecha_creacion= datetime.now() if not datos.fecha_creacion else datos.fecha_creacion,
+            fecha_creacion=datetime.now() if not datos.fecha_creacion else datos.fecha_creacion,
             tipo_de_descuento=datos.tipo_de_descuento,
             descripcion=datos.descripcion,
             fecha_actualizacion=datetime.now(),
@@ -33,10 +41,10 @@ class CrearDescuento:
                 return descuento_existente
 
         descuento = self.repo_crear_descuento.crear(descuento)
+        return descuento
 
-        print(descuento)
-        # * Actualización cuenta por pagar
-
+    def _actualizar_cuenta_por_pagar(self, descuento: Descuento):
+        """Actualización del total a pagar y el total de descuentos"""
         cuenta_por_pagar = self.repo_obtener_cuenta.obtener_por_id(descuento.id_cuenta_por_pagar)
         if not cuenta_por_pagar:
             raise ValueError(f"Cuenta por pagar con id {descuento.id_cuenta_por_pagar} no encontrada")
@@ -46,5 +54,3 @@ class CrearDescuento:
         cuenta_por_pagar.total_a_pagar = cuenta_por_pagar.total_a_pagar
 
         self.repo_actualizar_cuenta.actualizar(cuenta_por_pagar)
-
-        return descuento
